a0dd718bc999f68dcfb9ff978b7b6b10530be457
Ivan Shapovalov <intelfx100@gmail.com>
2013-11-30 17:28:01 +0400

    dropbox_find_conflicts.sh: semi-rewrite.

--- dropbox_find_conflicts.sh
+++ dropbox_find_conflicts.sh
@@ -14,18 +14,12 @@ function log_op() {
 
 while read conflict_file; do
 	initial_file=${conflict_file/ ([^ ]*\'s conflicted copy*)}
-	if [ ! -f "$initial_file" ]; then
+	if (( FORCE )) || [ ! -f "$initial_file" ] || [ "$conflict_file" -nt "$initial_file" ]; then
 		mv "$conflict_file" "$initial_file"
-		log_op "RENAME" "$conflict_file" "$initial_file" | tee "$TMPFILE"
-
-	elif [ "$conflict_file" -nt "$initial_file" ]; then
-		mv "$conflict_file" "$initial_file"
-		log_op "MOVE" "$conflict_file" "$initial_file" | tee "$TMPFILE"
-
-	elif [ "$conflict_file" -ot "$initial_file" ]; then
+		log_op "USE PRE-SYNC (mv)" "$conflict_file" "$initial_file" | tee "$TMPFILE"
+	else
 		install -D "$conflict_file" "$TMPDIR/$conflict_file"
 		rm "$conflict_file"
-		log_op "REMOVE" "$conflict_file" | tee $TMPFILE
-		
+		log_op "USE POST-SYNC (rm)" "$conflict_file" | tee $TMPFILE
 	fi
 done < <(find -L ~/Dropbox -name '*conflicted*' -print )
