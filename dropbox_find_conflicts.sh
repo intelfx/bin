db7c3775326d4c1682a3b0c411032f415238afc8
Ivan Shapovalov <intelfx100@gmail.com>
2014-06-27 19:19:21 +0400

    dropbox_find_conflicts.sh: correctly handle directories, permissions, etc (mkdir+mv instead of install).

--- dropbox_find_conflicts.sh
+++ dropbox_find_conflicts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-TMPFILE=~/tmp/find_conflicts.txt
-TMPDIR=~/tmp/find_conflicts
-mkdir -p $TMPDIR
+TMPFILE="$HOME/tmp/find_conflicts.txt"
+TMPDIR="$HOME/tmp/find_conflicts"
+mkdir -p "$TMPDIR"
 
 function log_op() {
 	echo "$1:"
@@ -16,11 +16,12 @@ while read conflict_file; do
 	initial_file=${conflict_file/ ([^ ]*\'s conflicted copy*)}
 	if (( FORCE )) || [ ! -f "$initial_file" ] || [ "$conflict_file" -nt "$initial_file" ]; then
 		log_op "USE PRE-SYNC (mv)" "$conflict_file" "$initial_file" | tee "$TMPFILE"
-		install -D "$initial_file" "$TMPDIR/$initial_file"
+		mkdir -p "$TMPDIR/$(dirname "$initial_file")"
+		mv "$initial_file" "$TMPDIR/$initial_file"
 		mv "$conflict_file" "$initial_file"
 	else
 		log_op "USE POST-SYNC (rm)" "$conflict_file" | tee $TMPFILE
-		install -D "$conflict_file" "$TMPDIR/$conflict_file"
-		rm "$conflict_file"
+		mkdir -p "$TMPDIR/$(dirname "$conflict_file")"
+		mv "$conflict_file" "$TMPDIR/$conflict_file"
 	fi
 done < <(find -L ~/Dropbox -name '*Конфликтующая копия*' -print )
