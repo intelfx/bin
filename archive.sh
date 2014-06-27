ada6bb93b8c58a1c079b36c1d06df7d2e90f840e
Ivan Shapovalov <intelfx100@gmail.com>
2014-06-27 19:18:25 +0400

    archive.sh: refactor, fix multiple arguments, make more human-friendly.

--- archive.sh
+++ archive.sh
@@ -10,8 +10,42 @@ ARCHIVERS=(
 	[bz2]="bzip2 -9"
 )
 
+function get_compressors() {
+	for compressor in "${!ARCHIVERS[@]}"; do
+		echo "$1$compressor: '${ARCHIVERS[$compressor]}'"
+	done
+}
+
 function get_size() {
-	du --apparent-size -sb "$@" | cut -d$'\t' -f1
+	du --apparent-size -csb "$@" | tail -n1 | cut -d$'\t' -f1
+}
+
+function usage() {
+	cat <<EOF
+archive.sh -- a directory archiving tool
+
+Usage: $0 [-fn] [-o OUTPUT] [-N NAME] [-c COMPRESSOR]
+
+Options:
+	-o OUTPUT
+		Write archive to OUTPUT; may be directory or file.
+
+	-N NAME
+		Use NAME for logging.
+		Also used as archive file name stem, if OUTPUT is a directory.
+
+	-c COMPRESSOR
+		Explicitly set compressing filter to COMPRESSOR.
+		Available options:
+$(get_compressors "		 * ")
+
+	-n
+		Explicitly disable compression.
+
+	-f
+		Allow overwriting the output file.
+
+EOF
 }
 
 ARGS=$(getopt -- "o:N:nc:f" "$@")
@@ -23,8 +57,7 @@ eval set -- "$ARGS"
 
 # Defaults.
 
-ARCHIVER='xz'
-DIRECTORY="$(pwd)"
+ARCHIVER=""
 PV_OPTS="-pba"
 PV_SECONDSTAGE_OPTS="-b"
 
@@ -53,16 +86,8 @@ while true; do
 			break
 			;;
 		*)
-			cat <<- EOF
-			The archiver.
-			Options:
-				-o <output directory or file>
-				-N <archive name>
-				-c <compressor name: ${!ARCHIVERS[@]}>
-				-n (do not compress)
-
-			Non-options shall be directory names and common options of "du" and "tar" (e. g. --exclude).
-			EOF
+			echo "E: wrong option."
+			usage
 			exit 1
 			;;
 	esac
@@ -71,36 +96,55 @@ while true; do
 done
 
 if ! (( $# )); then
-	echo "==== Error: no directory arguments given"
+	echo "E: No source files to archive."
+	usage
 	exit 1
 fi
-
-[[ -z "$NAME" ]] && NAME="$(basename "$1")"
 
 if [[ -d "$OUTPUT" ]]; then
 	OUTPUT+="/${NAME}.tar"
 else
+	if [[ -z "$NAME" ]]; then
+		if [[ -n "$OUTPUT" ]]; then
+			NAME="${OUTPUT##*/}" # use output as name: remove directories
+			NAME="${NAME%%.*}" # use output as name: remove extensions
+		else
+			NAME="${1##*/}" # use first source directory as name: remove directories
+		fi
+
+		echo "N: defaulting name to '$NAME'."
+	fi
+
+	if [[ -z "$OUTPUT" ]]; then
+		OUTPUT="${NAME}.tar" # use name as output
+
+		echo "N: defaulting output to '$OUTPUT'."
+	fi
+
 	if (( FORCE )) && [[ -e "$OUTPUT" ]]; then
-		echo "==== Warning: output ($OUTPUT) exists and --force is given, removing"
-		rm -vf "$OUTPUT"
+		echo "W: '$OUTPUT' exists and is not a directory. Overwriting."
+		rm -f "$OUTPUT"
 	fi
 
 	if [[ -e "$OUTPUT" ]]; then
-		echo "==== Error: output ($OUTPUT) exists and is not a directory"
+		echo "E: '$OUTPUT' exists and is not a directory. Pass '-f' to overwrite."
 		exit 1
 	fi
-	
-	if [[ -z "$OUTPUT" ]]; then
-		OUTPUT="${NAME}.tar"
-	fi
+
 fi
 
-echo "---- Destination file: $OUTPUT"
+if [[ -z "$ARCHIVER" && "$OUTPUT" =~ \.tar\.(.*)$ ]]; then
+	ARCHIVER="${BASH_REMATCH[1]}"
+
+	echo "N: defaulting archiver to '${BASH_REMATCH[1]}'."
+fi
 
 if [[ "$ARCHIVER" && "${ARCHIVERS["$ARCHIVER"]}" ]]; then
 	ARCHIVER_EXT="$ARCHIVER"
 	ARCHIVER_CMD="${ARCHIVERS["$ARCHIVER"]}"
 	ARCHIVER_NAME="${ARCHIVER%% *}" # Select first word of the string (command name)
+
+	echo "N: compressing and writing to '${OUTPUT}.${ARCHIVER_EXT}'"
 
 	tar -c "$@" \
 		| pv ${PV_OPTS}             -cN "Archiving   $NAME (tar)" -s $(get_size "$@") \
@@ -108,6 +152,8 @@ if [[ "$ARCHIVER" && "${ARCHIVERS["$ARCHIVER"]}" ]]; then
 		| pv ${PV_SECONDSTAGE_OPTS} -WN "Compressing $NAME ($ARCHIVER_NAME)" \
 		> "${OUTPUT}.${ARCHIVER_EXT}"
 else
+	echo "N: writing to '${OUTPUT}'"
+
 	tar -c "$@" \
 		| pv $PV_OPTS -N "$NAME" -s $(get_size "$@") \
 		> "${OUTPUT}"
