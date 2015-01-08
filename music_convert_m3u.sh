58e597d6ce320d2960a2ebb5a4cc7c960d5bb63d
Ivan Shapovalov <intelfx100@gmail.com>
2015-01-08 04:38:12 +0300

    music_convert_m3u.sh: refactor, convert using fdkaac, add nice progress bars.

--- music_convert_m3u.sh
+++ music_convert_m3u.sh
@@ -11,7 +11,50 @@ OUTPUT="$2"
 [[ -d "$OUTPUT" ]] || exit_err "Output is not a directory: '$(i_e "$OUTPUT")'"
 
 PREPARED="${INPUT}.tmp"
+TO_CONVERT="${INPUT}.to-convert"
+TO_COPY="${INPUT}.to-copy"
 grep -vE '^#' "$INPUT" > "$PREPARED"
+
+CONVERT_SCRIPT="convert_script.sh"
+cat > "$CONVERT_SCRIPT" <<"EOF"
+#!/bin/bash
+
+IN="$2"
+OUT="$2"
+OUT="$1/${OUT##*/}"
+OUT="${OUT%.*}"
+
+OUT_EXT="m4a"
+
+OUT_TMP="$(mktemp "$OUT.$OUT_EXT-XXXXXX")"
+
+function cleanup() {
+	rm -f "$OUT_TMP"
+}
+
+trap cleanup EXIT
+
+echo -n "$IN ... "
+{ ffmpeg -i "$IN" -f caf - | fdkaac -m 5 -G 2 -w 20000 - -o "$OUT_TMP"; } &>/dev/null
+
+mv -n "$OUT_TMP" "$OUT.$$-$RANDOM.$OUT_EXT"
+echo "done"
+EOF
+chmod +x "$CONVERT_SCRIPT"
+
+COPY_SCRIPT="copy_script.sh"
+cat > "$COPY_SCRIPT" <<"EOF"
+#!/bin/bash
+
+IN="$2"
+OUT="$2"
+OUT="$1/${OUT##*/}"
+OUT_EXT="${OUT##*.}"
+OUT="${OUT%.*}"
+
+cp -v "$IN" "$OUT.$$-$RANDOM.$OUT_EXT"
+EOF
+chmod +x "$COPY_SCRIPT"
 
 function lines_by_format() {
 	grep -iE "\\.${2}$" "${1}"
@@ -29,10 +72,21 @@ files_by_formats() {
 	done
 }
 
-echo "doing convert..."
-files_by_formats "$PREPARED" "${CONVERT_FORMATS[@]}" | xargs -d$'\n' soundkonverter --output "$OUTPUT"
+files_by_formats "$PREPARED" "${CONVERT_FORMATS[@]}" > "$TO_CONVERT"
+files_by_formats "$PREPARED" "${COPY_FORMATS[@]}" > "$TO_COPY"
 
-echo "doing cp..."
-files_by_formats "$PREPARED" "${COPY_FORMATS[@]}" | xargs -d$'\n' cp -t "$OUTPUT"
+function process_and_pv() {
+	local FILE_LIST="$1"
+	local NAME="$2"
+	shift 2
 
-rm "$PREPARED"
+	local FILE_COUNT="$(wc -l < "$FILE_LIST")"
+
+	xargs -d$'\n' --arg-file "$FILE_LIST" "$@" | pv -N "$NAME ($FILE_COUNT files)" -pbate -W -c -s "$FILE_COUNT" -l >/dev/null
+}
+
+process_and_pv "$TO_COPY" "copying" -n1 -P1 ./"$COPY_SCRIPT" "$OUTPUT" &
+process_and_pv "$TO_CONVERT" "converting" -n1 -P8 ./"$CONVERT_SCRIPT" "$OUTPUT" &
+wait
+
+rm -vf "$PREPARED" "$TO_COPY" "$TO_CONVERT" "$CONVERT_SCRIPT" "$COPY_SCRIPT"
