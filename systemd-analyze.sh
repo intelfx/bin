af720aa556c11a262ca85ec717862b94831b2364
Ivan Shapovalov <intelfx100@gmail.com>
2015-05-04 23:55:23 +0300

    Rename boot_analyze.sh -> systemd-analyze.sh for consistency.

--- systemd-analyze.sh
+++ systemd-analyze.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 SVG_PATH="/tmp/systemd-analyze.svg"
-MAIN_UNIT="lightdm.service"
+MAIN_UNIT="gdm.service"
 ACTION="$1"
 shift
 
 case "$ACTION" in
 	critical-chain)
-		exec systemd-analyze critical-chain "${1:-MAIN_UNIT}"
+		exec systemd-analyze critical-chain "${1:-$MAIN_UNIT}"
 		;;
 	plot)
 		systemd-analyze plot > "$SVG_PATH" || exit 1
-		exec konqueror "$SVG_PATH"
+		exec xdg-open "$SVG_PATH"
 		;;
 	dot)
 		systemd-analyze dot "$@" | dot -Tsvg > "$SVG_PATH" || exit 1
-		exec konqueror "$SVG_PATH"
+		exec xdg-open "$SVG_PATH"
 		;;
 	*)
 		exit 1
