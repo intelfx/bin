9f83b1041e891347659bc4e0f2581a1893807032
Ivan Shapovalov <intelfx100@gmail.com>
2015-06-11 21:26:29 +0300

    systemd-analyze.sh: use `firefox` as fixed viewer.

--- systemd-analyze.sh
+++ systemd-analyze.sh
@@ -3,6 +3,8 @@
 SVG_PATH="/tmp/systemd-analyze.svg"
 MAIN_UNIT="gdm.service"
 ACTION="$1"
+#SVG_APP="xdg-open"
+SVG_APP="firefox"
 shift
 
 case "$ACTION" in
@@ -11,11 +13,11 @@ case "$ACTION" in
 		;;
 	plot)
 		systemd-analyze plot > "$SVG_PATH" || exit 1
-		exec xdg-open "$SVG_PATH"
+		exec "${SVG_APP[@]}" "$SVG_PATH"
 		;;
 	dot)
 		systemd-analyze dot "$@" | dot -Tsvg > "$SVG_PATH" || exit 1
-		exec xdg-open "$SVG_PATH"
+		exec "${SVG_APP[@]}" "$SVG_PATH"
 		;;
 	*)
 		exit 1
