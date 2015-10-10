c337b9d2cc405b1cf736d212376a4a4d0b333f87
Ivan Shapovalov <intelfx100@gmail.com>
2015-10-10 23:57:49 +0300

    gnome-set-colors.sh: add a script for switching light/dark colors in various GNOME apps.

---
+++ gnome-set-colors.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# $1: whether to enable dark (1 or 0)
+function adjust_gtk3() {
+	git config --file="$HOME/.config/gtk-3.0/settings.ini" Settings.gtk-application-prefer-dark-theme "$1"
+}
+
+function adjust_gedit() {
+	local scheme=( solarized-light solarized-dark )
+
+	dconf write /org/gnome/gedit/preferences/editor/scheme "'${scheme[$1]}'"
+}
+
+function adjust_terminal() {
+	local profiles_default=( 4b05dbdf-11e8-4f29-b788-7016ec6b6de9
+                             b739b980-6bbc-4e0c-9dda-313ad61029ff )
+    dconf write /org/gnome/terminal/legacy/profiles:/default "'${profiles_default[$1]}'"
+}
+
+function adjust_builder() {
+	local night_mode=( false true )
+	local style_scheme_name=( solarized-light solarized-dark )
+
+	dconf write /org/gnome/builder/night-mode "${night_mode[$1]}"
+	dconf write /org/gnome/builder/editor/style-scheme-name "'${style_scheme_name[$1]}'"
+}
+
+case "${1:-1}" in
+	dark|on|1) set -- 1 ;;
+	light|off|0) set -- 0 ;;
+	*) echo "E: unknown mode: '$1'" >&2; exit 1 ;;
+esac
+
+adjust_gtk3 "$@"
+adjust_gedit "$@"
+adjust_terminal "$@"
