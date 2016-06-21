d664dcfe5369fe885ca563ac40de8f21f7f681c4
Ivan Shapovalov <intelfx@intelfx.name>
2016-06-21 10:24:20 +0300

    gnome-set-colors.sh: indentation fixes and kde/kate color schema adjustments.

--- gnome-set-colors.sh
+++ gnome-set-colors.sh
@@ -13,8 +13,8 @@ function adjust_gedit() {
 
 function adjust_terminal() {
 	local profiles_default=( 4b05dbdf-11e8-4f29-b788-7016ec6b6de9
-                             b739b980-6bbc-4e0c-9dda-313ad61029ff )
-    dconf write /org/gnome/terminal/legacy/profiles:/default "'${profiles_default[$1]}'"
+	                         b739b980-6bbc-4e0c-9dda-313ad61029ff )
+	dconf write /org/gnome/terminal/legacy/profiles:/default "'${profiles_default[$1]}'"
 }
 
 function adjust_builder() {
@@ -23,6 +23,21 @@ function adjust_builder() {
 
 	dconf write /org/gnome/builder/night-mode "${night_mode[$1]}"
 	dconf write /org/gnome/builder/editor/style-scheme-name "'${style_scheme_name[$1]}'"
+}
+
+function adjust_kde() {
+	local kdeglobals=( light dark )
+	local config_path="$(systemd-path user-configuration)"
+
+	cp -a "$config_path/kdeglobals-${kdeglobals[$1]}" "$config_path/kdeglobals"
+	kbuildsycoca5
+}
+
+function adjust_kate() {
+	local schema=( "Solarized (light)" "Solarized (dark)" )
+	local config_path="$(systemd-path user-configuration)/katepartrc"
+
+	sed -re "s|^Schema=.*|Schema=${schema[$1]}|" -i "$config_path"
 }
 
 case "${1:-1}" in
@@ -34,3 +49,5 @@ esac
 adjust_gtk3 "$@"
 adjust_gedit "$@"
 adjust_terminal "$@"
+adjust_kde "$@"
+adjust_kate "$@"
