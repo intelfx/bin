0d90499d7cff81281c2bd3832664989fd13db9e3
Ivan Shapovalov <intelfx@intelfx.name>
2018-04-21 17:27:16 +0300

    misc/owrt: opkg-get-user.sh: list actual user-installed packages in OpenWRT (sh/awk version)

---
+++ misc/owrt/opkg-get-user.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+OPKG_STATUS="/usr/lib/opkg/status"
+
+if [ "$1" ]; then
+	OPKG_STATUS="$1"
+fi
+
+AWK_FIND_MIN_TIME='
+BEGIN { min = systime(); FS = ": " }
+/^Installed-Time: / { min = min < $2 ? min : $2 }
+END { print min }
+'
+
+AWK_FILTER_PACKAGES='
+function pkg_new(pkg)
+{
+	package = pkg
+	p_user = 0
+	p_time = 0
+}
+
+function pkg_check()
+{
+	if (p_user && p_time) {
+		print package
+	}
+}
+
+BEGIN { FS = ": " }
+/^Package: / { pkg_new($2) }
+/^Status: .*\<user\>.*/ { p_user = 1; pkg_check() }
+/^Installed-Time: / { if ($2 > min_time) { p_time = 1; pkg_check() } }
+'
+
+function awk_opkg_status() {
+	awk "$@" "$OPKG_STATUS"
+}
+
+MIN_TIME="$(awk_opkg_status "$AWK_FIND_MIN_TIME")"
+awk_opkg_status -v min_time="$MIN_TIME" "$AWK_FILTER_PACKAGES"
