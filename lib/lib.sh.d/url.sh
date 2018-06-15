769df9fe1a00a87290ae15fe45bd7a398ed43d2f
Ivan Shapovalov <intelfx@intelfx.name>
2018-06-15 03:37:09 +0300

    lib, misc/wrappers: add a few more useful (no) scripts

---
+++ lib/lib.sh.d/url.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+# ref. https://unix.stackexchange.com/a/187256/21885
+#
+
+urlencode() {
+    # urlencode <string>
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf "$c" ;;
+            *) printf '%%%02X' "'$c"
+        esac
+    done
+}
+
+urldecode() {
+    # urldecode <string>
+
+    local url_encoded="${1//+/ }"
+    printf '%b' "${url_encoded//%/\\x}"
+}
