1cfbbd1289fb573a06ce7ade12eee6fbf00f2d46
Ivan Shapovalov <intelfx@intelfx.name>
2018-11-25 07:35:18 +0300

    ops: k8s: thaw-pods.sh: accept args for `kubectl get pods` and `kubectl delete pods`, separated with --

--- ops/k8s/thaw-pods.sh
+++ ops/k8s/thaw-pods.sh
@@ -1,5 +1,21 @@
 #!/bin/bash -e
 
-kubectl get pods "$@" -o json \
+KUBECTL_GET_ARGS=()
+KUBECTL_DELETE_ARGS=()
+
+while (( $# )); do
+  [[ $1 == -- ]] && { shift; break; }
+  KUBECTL_GET_ARGS+=( "$1" )
+  shift
+done
+
+while (( $# )); do
+  [[ $1 == -- ]] && { shift; break; }
+  KUBECTL_DELETE_ARGS+=( "$1" )
+  shift
+done
+
+
+kubectl get pods "${KUBECTL_GET_ARGS[@]}" -o json \
   | jq -r '.items[] | select(.status.containerStatuses[]?.ready == false) | .metadata.name' \
-  | xargs -r kubectl delete pods
+  | xargs -r kubectl delete pods "${KUBECTL_DELETE_ARGS[@]}"
