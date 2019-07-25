6ad4d2252d5f2a113f243f81e207d96946802199
Ivan Shapovalov <intelfx@intelfx.name>
2019-07-25 20:41:40 +0300

    ops: k8s: tmux-dashboard.sh: run stern in a loop, in a shell immune to SIGINT

--- ops/k8s/tmux-dashboard.sh
+++ ops/k8s/tmux-dashboard.sh
@@ -20,7 +20,7 @@ set -e
 SELECTOR=( "$@" )
 
 pane_stern=$(tmux new-window -P -F '#{pane_id}' -d -n "(k8s: ${SELECTOR[*]})" \
-  -- stern '.*' "${SELECTOR[@]}")
+  -- sh -c "trap : INT; while :; do stern --tail 0 '.*' ${SELECTOR[*]}; done")
 pane_toppods=$(tmux split-window -P -F '#{pane_id}' -t $pane_stern -v -b \
   -- watch -n30 kubectl top pods "${SELECTOR[@]}")
 pane_getpods=$(tmux split-window -P -F '#{pane_id}' -t $pane_toppods -h -b \
