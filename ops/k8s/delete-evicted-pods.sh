5a6becfaa580602e0f1208660f776c66b1b510a5
Ivan Shapovalov <intelfx@intelfx.name>
2020-02-20 04:43:22 +0300

    ops: k8s: delete-evicted-pods.sh: pass extra arguments both to `kubectl get pods` and `kubectl delete pods`

--- ops/k8s/delete-evicted-pods.sh
+++ ops/k8s/delete-evicted-pods.sh
@@ -2,4 +2,4 @@
 
 kubectl get pods "$@" -o json \
   | jq -r '.items[] | select(.status | (.phase == "Failed") and (.reason == "Evicted")) | .metadata.name' \
-  | xargs -r kubectl delete pods
+  | xargs -r kubectl delete pods "$@"
