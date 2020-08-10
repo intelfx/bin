e33c94b1eb187d1ad851142216679d0584697320
Ivan Shapovalov <intelfx@intelfx.name>
2020-08-11 01:01:29 +0300

    ops: k8s: helm/helm-init.sh: use helm2

--- ops/k8s/helm/helm-init.sh
+++ ops/k8s/helm/helm-init.sh
@@ -2,4 +2,4 @@
 
 HELM_INIT_ARGS=( "$@" )
 
-helm init --service-account tiller --override 'spec.template.spec.containers[0].command'='{/tiller,--storage=secret}' "${HELM_INIT_ARGS[@]}"
+helm2 init --service-account tiller --override 'spec.template.spec.containers[0].command'='{/tiller,--storage=secret}' "${HELM_INIT_ARGS[@]}"
