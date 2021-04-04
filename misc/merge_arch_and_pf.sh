e4ed26c2717b7b5546457dc660f72832225ec015
Ivan Shapovalov <intelfx@intelfx.name>
2021-04-05 00:40:40 +0300

    misc: merge_arch_and_pf.sh: refactor for robustness

--- misc/merge_arch_and_pf.sh
+++ misc/merge_arch_and_pf.sh
@@ -8,23 +8,46 @@ shopt -s lastpipe
 log "Fetching remotes"
 git fetch -j$(nproc) --multiple stable arch pf
 
-log "Determining versions"
-tag="$(git describe --match 'v*.*.*' --abbrev=0 stable/linux-rolling-stable)"
-log " Latest stable: $tag"
-arch_tag="$(git tag --list "$tag-arch*" | sed -nr 's|^.*-arch([0-9]+)$|\1\t\0|p' | sort -k1 -n | cut -f2 | tail -n1)"
-log " Latest -arch tag: $arch_tag"
-release="$(<<<"$tag" sed -nr 's|^v([0-9]+\.[0-9]+)\.[0-9]+$|\1|p')"
-log " Major release: $release"
+git_list_versions() {
+	# TODO: "rolling back" changes with sed is ugly
+	# separate the tag and the sorting key
+	git tag --list \
+		| grep -Ex 'v[0-9]+\.[0-9]+(\.[0-9]+)?' \
+		| sort -V "$@"
+}
 
-if ! [[ $tag && $arch_tag && $release ]]; then
-	die "Failed to parse tags / determine versions"
-fi
+git_list_versions_rc() {
+	# TODO: "rolling back" changes with sed is ugly
+	# separate the tag and the sorting key
+	git tag --list \
+		| grep -Ex 'v[0-9]+\.[0-9]+(.[0-9]+)?(-rc[0-9]+)?' \
+		| sed -r 's|-rc|~rc|' \
+		| sort -V "$@" \
+		| sed -r 's|~rc|-rc|'
+}
+
+git_verify() {
+	git rev-parse --verify --quiet "$1" >/dev/null
+}
+
+log "Determining versions"
+tag="$(git_list_versions | tail -n1)"
+git_verify "$tag" || die "Failed to determine latest stable, exiting"
+log " Latest stable: $tag"
+
+arch_tag="$(git tag --list "$tag-arch*" | grep -E -- '-arch[0-9]+$' | sort -V)"
+git_verify "$arch_tag" || die "Failed to determine latest -arch, exiting"
+log " Latest -arch tag: $arch_tag"
+
+release="$(<<<"$tag" sed -nr 's|^v([0-9]+\.[0-9]+)(\.[0-9]+)?$|\1|p')"
+git_verify "v$release" || die "Failed to determine major release, exiting"
+log " Major release: $release"
 
 eval "$(globaltraps)"
 
 log "Merging -pf"
 git checkout -f "$arch_tag"
-if ! git merge --no-commit "pf/pf-$release"; then
+if ! git merge --no-commit --no-rerere "pf/pf-$release"; then
 	ltrap 'git merge --abort'
 fi
 
@@ -33,7 +56,7 @@ git_list_conflicts() {
 }
 
 git_list_unstaged() {
-	git status  --untracked=no --porcelain | { grep -E '^.[^ ] ' || true; }
+	git status --untracked=no --porcelain | { grep -E '^.[^ ] ' || true; }
 }
 
 git_list_parse() {
@@ -44,7 +67,7 @@ git_list_parse() {
 }
 
 makefile_get_extraversion() {
-	<Makefile sed -nr 's|^EXTRAVERSION = -([^ ]+)$|\1|p' | head -n1
+	<Makefile sed -nr 's|^EXTRAVERSION = -([^ ]+)$|\1|p' | head -n1 | grep .
 }
 
 merge_makefile() {
@@ -55,11 +78,12 @@ merge_makefile() {
 		die "Could not parse -pf EXTRAVERSION"
 	fi
 
+	git checkout --ours Makefile
 	if ! ours_extraversion="$(makefile_get_extraversion)"; then
 		die "Could not parse main EXTRAVERSION"
 	fi
 
-	sed -r "s|^(EXTRAVERSION = ).*$|\1$ours_extraversion$pf_extraversion|" -i Makefile
+	sed -r "s|^(EXTRAVERSION = ).*$|\1-$ours_extraversion$pf_extraversion|" -i Makefile
 	git add Makefile
 }
 
@@ -92,7 +116,7 @@ if ! final_extraversion="$(makefile_get_extraversion)"; then
 fi
 final_tag="$tag-$final_extraversion"
 
-if git tag --list | grep -Fx "$final_tag" &>/dev/null; then
+if git_verify "$final_tag"; then
 	die "Tag $final_tag already exists, not overwriting"
 fi
 log "Tagging as $final_tag"
