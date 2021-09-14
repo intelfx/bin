c13771ef5d5e97c027f20442565c246c4748fc9a
Ivan Shapovalov <intelfx@intelfx.name>
2021-09-15 00:35:51 +0300

    landfill: untouch-dir.sh: accept multiple arguments

--- landfill/untouch-dir.sh
+++ landfill/untouch-dir.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-dir="$1"
+. lib.sh || exit 1
 
-most_recent_mtime="$(find "$dir" -mindepth 1 -maxdepth 1 -printf '%T@\n' | sort -n | tail -n1)"
-[[ "$most_recent_mtime" ]] || exit 1
-touch -d "@$most_recent_mtime" "$dir"
+rc=0
+for dir; do
+	if ! [[ -d "$dir" ]]; then
+		err "$dir: not a directory, skipping"
+		rc=1
+	fi
+	most_recent_mtime="$(find "$dir" -mindepth 1 -maxdepth 1 -printf '%T@\n' | sort -n | tail -n1)" || { rc=1; continue; }
+	if ! [[ "$most_recent_mtime" ]]; then
+		log "$dir: no files, skipping"
+		continue
+	fi
+	touch -d "@$most_recent_mtime" "$dir" || { rc=1; continue; }
+done
+exit $rc
