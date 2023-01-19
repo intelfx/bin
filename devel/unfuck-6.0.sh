48f5b280dd6f24d2614ad639cf043ba97ed07d9d
Ivan Shapovalov <intelfx@intelfx.name>
2023-01-19 13:49:26 +0300

    devel: unfuck-6.0.sh: explicitly target v6.0.y and bcachefs-6.0

--- devel/unfuck-6.0.sh
+++ devel/unfuck-6.0.sh
@@ -2,16 +2,17 @@ set -eo pipefail
 shopt -s lastpipe
 set -x
 
-~/bin/devel/merge_arch_and_pf.sh "$@"
+~/bin/devel/merge_arch_and_pf.sh --major v6.0 "$@"
 git branch -f "build"
 
 git describe --tags --exact | grep -Eo '[0-9]+\.[0-9]+(\.[0-9]+)?' | read branch
 
-git merge-repeatedly bcachefs/master
+git merge-repeatedly bcachefs/bcachefs-v6.0
 #git cherry-pick fca6b6a74180  # mm/memcontrol.c: convert to printbuf, fix up merge
 #git cherry-pick 0e3d949b156e  # stacktrace: export stack_trace_save_tsk for bcachefs
 #git cherry-pick e3d42e2d6d65  # lib: export errname for bcachefs
 #git cherry-pick 55fda0a14d3a  # mm: filemap: export mapping_seek_hole_data() for bcachefs
 git branch -f "work/patch-$branch"
+git branch -f "work/patch-6.0"
 git branch -f "work/patch"
 
