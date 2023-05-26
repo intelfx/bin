f2a3e616554e040e6e04942abd91a1e9a1e45165
Ivan Shapovalov <intelfx@intelfx.name>
2023-05-26 15:00:32 +0300

    local: tmux-ryzen-dashboard-bench.sh: add a variant of tmux-ryzen-dashboard.sh for running tests

---
+++ local/tmux-ryzen-dashboard-bench.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+
+#
+# tmux-ryzen-dashboard-bench.sh -- spawns a new tmux window with a stress test
+#                                  and `ryzen_monitor` + `liquidctl` in watch(1)
+#                                  in separate panes.
+#
+# ---------------------------------------
+# |               |         |           |
+# |               |         |           |
+# |               |         |           |
+# | ryzen_monitor |  bench  | liquidctl |
+# |               |         |           |
+# |               |         |           |
+# |               |         |           |
+# ---------------------------------------
+#
+
+cd "$HOME"
+
+pane_shell=$(tmux new-window -P -F '#{pane_id}' -d -n "(dashboard)" \
+  )
+pane_ryzen=$(tmux split-window -P -F '#{pane_id}' -t $pane_shell -h -b \
+  -- sudo ryzen_monitor)
+pane_liquidctl=$(tmux split-window -P -F '#{pane_id}' -t $pane_shell -h \
+  -- sudo watch -n1 "liquidctl status")
+
+tmux bind-key R respawn-pane
+tmux set-option -t $pane_shell remain-on-exit on
+tmux resize-pane -t $pane_ryzen -x 100  # exact=98
+tmux resize-pane -t $pane_liquidctl -x 55  # exact=52
+tmux respawn-pane -t $pane_shell -k "./bench.sh"
+tmux select-pane -t $pane_shell
