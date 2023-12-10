6b4041c50f92fe22f06d4b6c8c339a1345d2a5f9
Ivan Shapovalov <intelfx@intelfx.name>
2023-12-10 18:22:23 +0100

    devel: add unfuck-6.6.sh

---
+++ devel/unfuck-6.6.sh
@@ -0,0 +1,16 @@
+set -eo pipefail
+shopt -s lastpipe
+set -x
+
+~/bin/devel/merge_arch_and_pf.sh --major v6.6 "$@"
+git branch -f "build"
+
+git describe --tags --exact | grep -Eo '[0-9]+\.[0-9]+(\.[0-9]+)?' | read branch
+
+export GIT_AUTHOR_DATE="@0 +0000" GIT_COMMITTER_DATE="@0 +0000"
+git merge-repeatedly --no-edit bcachefs-hist/6.6
+
+git branch -f "work/patch-$branch"
+git branch -f "work/patch-6.6"
+git branch -f "work/patch"
+
