28ee7763178765a7d5aff035266d0f2bfd23a74e
Ivan Shapovalov <intelfx@intelfx.name>
2024-04-13 12:05:06 +0300

    local: tmux-setup.sh: use tmux-respawn-helper

--- local/tmux-setup.sh
+++ local/tmux-setup.sh
@@ -7,8 +7,11 @@
 # double nested tmux in gnome-terminal in gnome-shell on 2560x1440 at Iosevka@9 at 90%
 tmux set-option default-size 255x47
 
+respawn_helper="$HOME/bin/misc/tmux-respawn-helper"
+
 # create new windows
-tmux new-window 'sudo htop'
+tmux new-window -n "(htop)" \
+  -- "$respawn_helper" sudo htop
 ~/bin/local/tmux-ryzen-dashboard.sh
 
 # destroy the automatically created window with a shell
