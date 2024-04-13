028b2ea924234a65c069764d1bc6e18127705e8e
Ivan Shapovalov <intelfx@intelfx.name>
2024-04-13 12:10:16 +0300

    local: tmux-ryzen-dashboard{,-bench}.sh: use tmux-respawn-helper

--- local/tmux-ryzen-dashboard.sh
+++ local/tmux-ryzen-dashboard.sh
@@ -16,15 +16,16 @@
 #
 
 cd "$HOME"
+respawn_helper="$HOME/bin/misc/tmux-respawn-helper"
 
 pane_shell=$(tmux new-window -P -F '#{pane_id}' -d -n "(dashboard)" \
   )
 pane_ryzen=$(tmux split-window -P -F '#{pane_id}' -t $pane_shell -h -b \
-  -- sudo ryzen_monitor)
+  -- "$respawn_helper" sudo ryzen_monitor)
 pane_liquidctl=$(tmux split-window -P -F '#{pane_id}' -t $pane_shell -h \
-  -- sudo watch -n1 "liquidctl status")
+  -- "$respawn_helper" sudo watch -n1 "liquidctl status")
 pane_sensors=$(tmux split-window -P -F '#{pane_id}' -t $pane_shell -v -b \
-  -- watch -n1 "sensors 'nct6798-*' 'nvme-*' 'drivetemp-*'")
+  -- "$respawn_helper" watch -n1 "sensors 'nct6798-*' 'nvme-*' 'drivetemp-*'")
 
 tmux resize-pane -t $pane_ryzen -x 100  # exact=98
 tmux resize-pane -t $pane_liquidctl -x 55  # exact=52
