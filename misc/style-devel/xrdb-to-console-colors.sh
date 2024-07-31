384d84e8dfc5f887bc89f810601ea98dd21eb203
Ivan Shapovalov <intelfx@intelfx.name>
2024-07-31 03:43:13 +0200

    misc/style-devel: xrdb-to-console-colors.sh: run `xrdb` ourselves

--- misc/style-devel/xrdb-to-console-colors.sh
+++ misc/style-devel/xrdb-to-console-colors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -nre 's|^\*color([0-9]*):.*#(.*)$|\1 \2|p' | while read n color; do
+xrdb -E "$@" | sed -nre 's|^\*color([0-9]*):.*#(.*)$|\1 \2|p' | while read n color; do
 	echo $'\033]P'"$(printf '%X' "$n")$color"
 done
 
