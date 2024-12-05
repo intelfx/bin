8335d34285bc51e1b20fb51abb4706ec5418eac1
Ivan Shapovalov <intelfx@intelfx.name>
2024-12-05 15:02:36 +0300

    devel: zfs-copy-builtin.sh: add custom cleanups and `git commit`

--- devel/zfs-copy-builtin.sh
+++ devel/zfs-copy-builtin.sh
@@ -19,6 +19,12 @@ then
 	exit 1
 fi >&2
 
+if git -C "$KERNEL_DIR" status --porcelain | grep -q '.'; then
+	echo "$0: kernel tree ($KERNEL_DIR) is dirty, aborting" >&2
+
+	exit 1
+fi
+
 make clean ||:
 make gitrev
 
@@ -27,24 +33,22 @@ cp -R include "$KERNEL_DIR/include/zfs"
 cp -R module "$KERNEL_DIR/fs/zfs"
 cp zfs_config.h "$KERNEL_DIR/include/zfs/"
 
-cat > "$KERNEL_DIR/fs/zfs/Kconfig" <<EOF
-config ZFS
-	tristate "ZFS filesystem support"
-	depends on EFI_PARTITION
-	select ZLIB_INFLATE
-	select ZLIB_DEFLATE
-	help
-	  This is the ZFS filesystem from the OpenZFS project.
+echo "$0: cleaning up and adding copied ZFS sources to the kernel tree ($KERNEL_DIR)" >&2
+set -x
 
-	  See https://github.com/openzfs/zfs
+find "$KERNEL_DIR/fs/zfs" "$KERNEL_DIR/include/zfs" \( -name 'Makefile*' -or -name 'Kbuild.*' \) -exec rm -vf {} \+
+mv -vf "$KERNEL_DIR/fs/zfs/Kbuild" "$KERNEL_DIR/fs/zfs/Makefile"
+sed -r '/zfs_gitrev\.h/d' -i "$KERNEL_DIR/include/zfs/.gitignore"
+sed -r '/Kbuild/d' -i "$KERNEL_DIR/fs/zfs/.gitignore"
 
-	  To compile this file system support as a module, choose M here.
-
-	  If unsure, say N.
-EOF
-
-sed -i '/source "fs\/ext2\/Kconfig\"/i\source "fs/zfs/Kconfig"' "$KERNEL_DIR/fs/Kconfig"
-echo 'obj-$(CONFIG_ZFS) += zfs/' >> "$KERNEL_DIR/fs/Makefile"
+git -C "$KERNEL_DIR" add \
+	fs/zfs \
+	include/zfs \
+	# EOL
+git -C "$KERNEL_DIR" commit \
+	-m "zfs: add $(git describe --long --tags) ($(git show --no-patch --format='"%s"'))"
+{ set +x; } &>/dev/null
 
 echo "$0: done. now you can build the kernel with ZFS support." >&2
+echo "$0: don't forget to add Kbuild integration (c3373c78b9dcbe5cabd94fcba2bcabd3464f6784)." >&2
 echo "$0: make sure you enable ZFS support (CONFIG_ZFS) before building." >&2
