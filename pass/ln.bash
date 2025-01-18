7aa7ac96646991e45829bf37a5bec368039e5cb2
Ivan Shapovalov <intelfx@intelfx.name>
2025-01-18 18:22:32 +0400

    pass: ln.bash: unbreak and actually finish implementation

--- pass/ln.bash
+++ pass/ln.bash
@@ -18,7 +18,7 @@
 
 VERSION="0.1.0"
 
-cmd_either_show() {
+cmd_ln() {
 	local opts target=()
 	opts="$($GETOPT -o s -l symbolic -n "$PROGRAM" -- "$@")"
 	local err=$?
@@ -32,19 +32,41 @@ cmd_either_show() {
 	[[ $err -ne 0 || $# -ne 2 ]] && die "Usage: $PROGRAM $COMMAND [-s|--symbolic] src dest"
 
 	local src="$1" dest="$2"
+	check_sneaky_paths "$src"
+	check_sneaky_paths "$dest"
 
-	git
+	local srcpath destpath
+	srcpath="$(realpath -q --strip "$PREFIX/$src")"
+	destpath="$(realpath -q --strip "$PREFIX/$dest")"
+	set_git "$destpath"
 
-	local f passfile
+	local srcname
+	srcname="$(basename "$srcpath")"
 
-	for f; do
-		passfile="$PREFIX/$f.gpg"
+	local srcrel destrel
+	srcrel="${srcpath#"$PREFIX/"}"
+	destrel="${destpath#"$PREFIX/"}"
 
-		[[ -f $passfile ]] || continue
-
-		cmd_show "${target[@]}" "$f"
-		break
-	done
+	if [[ -f "$srcpath.gpg" ]]; then
+		if [[ -d "$destpath" ]]; then
+			ln -rsf "$srcpath.gpg" -T "$destpath/$srcname.gpg"
+			git_add_file "$destpath/$srcname.gpg" "Link $srcrel to $destrel/$srcname."
+		else
+			ln -rsf "$srcpath.gpg" -T "$destpath.gpg"
+			git_add_file "$destpath.gpg" "Link $srcrel to $destrel."
+		fi
+	elif [[ -d "$srcpath" ]]; then
+		if [[ -d "$destpath" ]]; then
+			die "Error: cannot link $srcrel to already existng directory $destrel."
+		elif [[ -e "$destpath.gpg" ]]; then
+			die "Error: cannot link $srcrel to already existing file $destrel."
+		else
+			ln -rsf "$srcpath" -T "$destpath"
+			git_add_file "$destpath" "Link $srcrel to $destrel."
+		fi
+	else
+		die "Error: $srcrel is not in the password store."
+	fi
 }
 
 cmd_ln "$@"
