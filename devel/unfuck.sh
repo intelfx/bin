5ed096db97f8689ea46d9c2bfcf28e3ef0ab3e53
Ivan Shapovalov <intelfx@intelfx.name>
2025-03-12 12:34:18 +0300

    devel: unfuck.sh: add 6.13 patches

--- devel/unfuck.sh
+++ devel/unfuck.sh
@@ -197,6 +197,29 @@ v6.12)
 		work/zfs-6.12 \
 		work/cpupower-6.12 \
 	;;
+
+v6.13)
+	# main
+	make_merge \
+		work/iwlwifi-lar-v2-6.12 \
+		work/amd-energy-6.12 \
+		work/btrfs-remove-ghost-subvolume-6.12 \
+		work/btrfs-allocation-hint-6.12 \
+		work/tsc-directsync-6.12 \
+		work/no-jobserver-exec-6.12 \
+		work/gvt-failsafe-6.12 \
+		work/gvt-workaround-6.12 \
+		work/i915-fastboot-revert-6.13 \
+		work/kbuild-6.12 \
+		work/em7565-ids-6.12 \
+		work/zswap-writeback-6.12 \
+		work/acpi-osc-6.12 \
+		work/fs-6.13 \
+		work/cddl-6.12 \
+		work/zfs-6.12 \
+		work/cpupower-6.12 \
+		# work/btrfs-6.13 \
+	;;
 esac
 
 Trace git branch -f "${target[patch_prefix]}${minor}"
