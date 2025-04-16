ad05f931e043892fc08d8943fb2a7d5e4b6520c3
Ivan Shapovalov <intelfx@intelfx.name>
2025-04-16 20:32:17 +0300

    landfill: untouch-dir.sh: implement recursive processing
    No more `find -depth -type d -exec ~/bin/landfill/untouch-dir.sh {} \;`.

--- landfill/untouch-dir.sh
+++ landfill/untouch-dir.sh
@@ -1,29 +1,72 @@
-#!/bin/bash
+. lib.sh || exit
 
-. lib.sh || exit 1
+_usage() {
+	cat <<EOF
+Usage: $0 [-f|--force]
+EOF
+}
 
-ARG_FORCE=0
-if [[ $1 == --force ]]; then
-	ARG_FORCE=1
-	shift
-fi
 
-rc=0
-for dir; do
-	if ! [[ -d "$dir" ]]; then
-		err "$dir: not a directory, skipping"
-		rc=1
-	fi
-	most_recent_mtime="$(find "$dir" -mindepth 1 -maxdepth 1 -printf '%T@\n' | sort -n | tail -n1)" || { rc=1; continue; }
-	if ! [[ "$most_recent_mtime" ]]; then
+#
+# args
+#
+
+declare -A _args=(
+	[-h|--help]=ARG_USAGE
+	[-f|--force]=ARG_FORCE
+	[--]=DIRS
+)
+parse_args _args "$@" || usage
+[[ ! $ARG_USAGE ]] || usage
+export ARG_FORCE
+
+
+#
+# functions
+#
+
+untouch_one_dir() {
+	set -eo pipefail
+	shopt -s lastpipe
+
+	local dir="$1"
+	[[ -d "$dir" ]] || { err "$dir: not a directory, skipping"; return 1; }
+
+	find "$dir" -mindepth 1 -maxdepth 1 -printf '%T@\n' \
+	| sort -n \
+	| tail -n1 \
+	| { IFS='' read -r mtime ||:; } \
+		|| return 1
+
+	if ! [[ $mtime ]]; then
 		if (( ARG_FORCE )); then
 			log "$dir: no files, resetting to epoch"
-			most_recent_mtime=0
+			mtime=0
 		else
 			log "$dir: no files, skipping"
-			continue
+			return 0
 		fi
 	fi
-	touch -d "@$most_recent_mtime" "$dir" || { rc=1; continue; }
+	touch -d "@$mtime" "$dir" || return 1
+}
+libsh_export_log
+export -f untouch_one_dir
+
+
+#
+# main
+#
+
+rc=0
+for dir in "${DIRS[@]}"; do
+	if ! [[ -d "$dir" ]]; then
+		rc=1
+		err "$dir: not a directory, skipping"
+		continue
+	fi
+	if ! find "$dir" -depth -type d | parallel -j1 'untouch_one_dir'; then
+		rc=$?
+		err "$dir: failed to process"
+	fi
 done
 exit $rc
