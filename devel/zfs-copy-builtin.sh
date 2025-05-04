54997c6a20dbf745d7643d695494ddbad52bd95f
Ivan Shapovalov <intelfx@intelfx.name>
2025-05-04 05:58:31 +0300

    devel: zfs-copy-builtin.sh: use common header

--- devel/zfs-copy-builtin.sh
+++ devel/zfs-copy-builtin.sh
@@ -1,28 +1,60 @@
-#!/bin/sh
+#!/bin/bash
 
-set -ef
+set -eo pipefail
+shopt -s lastpipe
 
-usage()
-{
-	echo "usage: $0 <kernel source tree>" >&2
+log() {
+	echo "${0##*/}: $*" >&2
+}
+
+err() {
+	echo "${0##*/}: error: $*" >&2
+}
+
+die() {
+	err "$@"
 	exit 1
 }
 
-[ "$#" -eq 1 ] || usage
-KERNEL_DIR="$1"
+usage() {
+	if (( $# )); then
+		echo "${0##*/}: $*" >&2
+		echo >&2
+	fi
+	_usage >&2
+	exit 1
+}
 
-if ! [ -e 'zfs_config.h' ]
-then
-	echo "$0: you did not run configure, or you're not in the ZFS source directory."
-	echo "$0: run configure with --with-linux=$KERNEL_DIR and --enable-linux-builtin."
+_usage() {
+	cat <<EOF
+usage: $0 <kernel source tree>
+EOF
+}
 
+
+#
+# args
+#
+
+case "$#" in
+1) KERNEL_DIR="$1" ;;
+*) usage "wrong number of positional arguments" ;;
+esac
+
+
+#
+# main
+#
+
+if ! [[ -e zfs_config.h ]]; then
+	err "you did not run configure, or you're not in the ZFS source directory."
+	err "run configure with --with-linux=$KERNEL_DIR and --enable-linux-builtin."
+	log "./autogen.sh && ./configure --prefix=/usr --with-config=all --with-linux=$KERNEL_DIR --enable-linux-experimental --enable-linux-builtin=yes --disable-debug && $0 $KERNEL_DIR"
 	exit 1
 fi >&2
 
 if git -C "$KERNEL_DIR" status --porcelain | grep -q '.'; then
-	echo "$0: kernel tree ($KERNEL_DIR) is dirty, aborting" >&2
-
-	exit 1
+	die "kernel tree ($KERNEL_DIR) is dirty, aborting"
 fi
 
 make clean ||:
@@ -33,7 +65,7 @@ cp -R include "$KERNEL_DIR/include/zfs"
 cp -R module "$KERNEL_DIR/fs/zfs"
 cp zfs_config.h -t "$KERNEL_DIR/include/zfs"
 
-echo "$0: cleaning up and adding copied ZFS sources to the kernel tree ($KERNEL_DIR)" >&2
+log "cleaning up and adding copied ZFS sources to the kernel tree ($KERNEL_DIR)"
 set -x
 
 find "$KERNEL_DIR/fs/zfs" "$KERNEL_DIR/include/zfs" \( -name 'Makefile*' -or -name 'Kbuild.*' \) -exec rm -vf {} \+
@@ -49,6 +81,6 @@ git -C "$KERNEL_DIR" commit \
 	-m "zfs: add $(git describe --long --tags) ($(git show --no-patch --format='"%s"'))"
 { set +x; } &>/dev/null
 
-echo "$0: done. now you can build the kernel with ZFS support." >&2
-echo "$0: don't forget to add Kbuild integration (c3373c78b9dcbe5cabd94fcba2bcabd3464f6784)." >&2
-echo "$0: make sure you enable ZFS support (CONFIG_ZFS) before building." >&2
+log "done. now you can build the kernel with ZFS support."
+log "don't forget to add Kbuild integration (c3373c78b9dcbe5cabd94fcba2bcabd3464f6784)."
+log "make sure you enable ZFS support (CONFIG_ZFS) before building."
