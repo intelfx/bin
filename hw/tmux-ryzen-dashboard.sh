3429fed8272d7594dd9c42bfa764933a294e9bfc
Ivan Shapovalov <intelfx@intelfx.name>
2025-12-17 04:36:54 +0300

    hw: tmux-ryzen-dashboard.sh: swap liquidctl with chrony, because I run an NTP server now I guess

--- hw/tmux-ryzen-dashboard.sh
+++ hw/tmux-ryzen-dashboard.sh
@@ -8,7 +8,7 @@
 # |               |         |           |
 # |               | sensors |           |
 # |               |         |           |
-# | ryzen_monitor |---------| liquidctl |
+# | ryzen_monitor |---------|  chrony   |
 # |               |         |           |
 # |               |  shell  |           |
 # |               |         |           |
@@ -22,13 +22,13 @@ pane_shell=$(tmux new-window -P -F '#{pane_id}' -d -n "(dashboard)" \
   )
 pane_ryzen=$(tmux split-window -P -F '#{pane_id}' -t $pane_shell -h -b \
   -- "$respawn_helper" sudo ryzen_monitor)
-pane_liquidctl=$(tmux split-window -P -F '#{pane_id}' -t $pane_shell -h \
-  -- "$respawn_helper" sudo watch -n1 "liquidctl status")
+pane_chrony=$(tmux split-window -P -F '#{pane_id}' -t $pane_shell -h \
+  -- "$respawn_helper" sudo watch -n1 "chronyc -m tracking sources sourcestats rtcdata")
 pane_sensors=$(tmux split-window -P -F '#{pane_id}' -t $pane_shell -v -b \
   -- "$respawn_helper" watch -n1 "sensors 'nct6798-*' 'nvme-*' 'drivetemp-*'")
 
 tmux resize-pane -t $pane_ryzen -x 100  # exact=98
-tmux resize-pane -t $pane_liquidctl -x 55  # exact=52
+tmux resize-pane -t $pane_chrony -x 80
 tmux resize-pane -t $pane_shell -y '25%'
 tmux select-pane -t $pane_shell
-tmux bind-key '0' resize-pane -t $pane_ryzen -x 100 \\\; resize-pane -t $pane_liquidctl -x 55 \\\; resize-pane -t $pane_shell -y '25%' \\\; select-pane -t $pane_shell
+tmux bind-key '0' resize-pane -t $pane_ryzen -x 100 \\\; resize-pane -t $pane_chrony -x 80 \\\; resize-pane -t $pane_shell -y '25%' \\\; select-pane -t $pane_shell
