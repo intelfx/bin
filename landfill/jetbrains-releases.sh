0a85e26999ba568df324e3889badc9d9ddf1b3d7
Ivan Shapovalov <intelfx@intelfx.name>
2026-03-14 18:22:25 +0100

    landfill: jetbrains-releases.sh: refactor, add `--json` for raw json dump

--- landfill/jetbrains-releases.sh
+++ landfill/jetbrains-releases.sh
@@ -3,8 +3,38 @@
 set -eo pipefail
 shopt -s lastpipe
 
-if (( $# )); then
-	RELEASE_TYPES_RE="$(IFS='|'; echo "$*")"
+# shellcheck source=../lib/lib.sh
+. lib.sh
+
+_usage() {
+	cat <<EOF
+Usage: $0 [--json] [RELEASE-TYPES...]
+
+Options:
+  --json	Output raw JSON instead of a table
+EOF
+	exit 1
+}
+
+RELEASE_TYPES=()
+ARG_JSON=0
+while (( $# )); do
+	case "$1" in
+	--json)
+		ARG_JSON=1
+		;;
+	-*)
+		usage "unknown option: ${1@Q}"
+		;;
+	*)
+		RELEASE_TYPES+=("$1")
+		;;
+	esac
+	shift
+done
+
+if [[ ${RELEASE_TYPES+set} ]]; then
+	RELEASE_TYPES_RE="$(IFS='|'; echo "${RELEASE_TYPES[*]}")"
 else
 	RELEASE_TYPES_RE=".*"
 fi
@@ -13,7 +43,7 @@ PRODUCTS_LIST=(RR PCC PCP CL DG WS GO)
 LAST_N=5
 
 # shellcheck disable=SC2016
-JQ_PROG='
+JQ_PROG_FIRST='
 | map({ code, name, releases })
 | map(select(.code | IN($products | split(" ") | .[])))
 | map(.releases |= (.
@@ -26,19 +56,55 @@ JQ_PROG='
 	| reverse
 	| .[0:$last_n]
 ))
+'
+
+# shellcheck disable=SC2016
+JQ_PROG_TABLE='
+| ($fields_list | split(" ")) as $fields
 | map(. as $o | .releases | map(. as $r
-	| "\($o.code)\t\($o.name)\t\($r.type)\t\($r.date)\t\($r.version)\t\($r.build)"
+	| ($o + $r) as $combined
+	| $fields | map($combined[.])
+	| join("\t")
 ))
 | flatten
 '
 
-COLUMNS=(CODE NAME TYPE DATE VERSION BUILD)
-{
-	(IFS=$'\t'; echo "${COLUMNS[*]}")
+# shellcheck disable=SC2016
+JQ_PROG_RAW='
+| map(.releases |= (.
+	| map(.
+		| del(.whatsnew, .uninstallFeedbackLinks, .licenseRequired)
+		| .patches |= ({ unix })
+		| .downloads |= ({ linux, linuxARM64 })
+	)
+))
+'
+
+do_fetch_process() {
+	local jq_prog="${@: -1}"
+	local -a jq_args=("${@:1:$#-1}")
+
 	curl -fsS 'https://data.services.jetbrains.com/products' \
 		| jq \
 			--arg products "${PRODUCTS_LIST[*]}" \
 			--arg release_types "$RELEASE_TYPES_RE" \
 			--argjson last_n "$LAST_N" \
-			". $JQ_PROG []" -r
+			"${jq_args[@]}" \
+			". $JQ_PROG_FIRST $jq_prog"
+}
+
+if (( ARG_JSON )); then
+	do_fetch_process \
+		"$JQ_PROG_RAW"
+	exit
+fi
+
+FIELDS=(code name type date version build)
+COLUMNS=(CODE NAME TYPE DATE VERSION BUILD)
+{
+	(IFS=$'\t'; echo "${COLUMNS[*]}")
+	do_fetch_process \
+		--arg fields_list "${FIELDS[*]}" \
+		-r \
+		"$JQ_PROG_TABLE []"
 } | column -Lt -s $'\t'
