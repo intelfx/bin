c23ee8bfdbfda8d44e40bade1cda5604b98fc1dd
Ivan Shapovalov <intelfx@intelfx.name>
2026-03-14 17:38:36 +0100

    landfill: jetbrains-releases.sh: cleanup jq script

--- landfill/jetbrains-releases.sh
+++ landfill/jetbrains-releases.sh
@@ -12,22 +12,25 @@ fi
 PRODUCTS_LIST=(RR PCC PCP CL DG WS GO)
 LAST_N=5
 
-JQ_PROG='.
-| map({code,name,releases})
+# shellcheck disable=SC2016
+JQ_PROG='
+| map({ code, name, releases })
 | map(select(.code | IN($products | split(" ") | .[])))
-| map(.releases |= map(select(.build)))
+| map(.releases |= (.
+	| map(select(.build))
+	| map(select(.type | match($release_types)))
+))
 | map(select(.releases | length > 0))
 | map(.releases |= (.
-	| map({type, date, majorVersion, version, build})
-	| map(select(.type | match($release_types)))
-	| sort_by(.build|split(".")|map(tonumber))
+	| sort_by(.build | split(".") | map(tonumber))
 	| reverse
 	| .[0:$last_n]
 ))
-| map(. as $o | .releases | map(. as $r |
-	"\($o.code)\t\($o.name)\t\($r.type)\t\($r.date)\t\($r.version)\t\($r.build)"
+| map(. as $o | .releases | map(. as $r
+	| "\($o.code)\t\($o.name)\t\($r.type)\t\($r.date)\t\($r.version)\t\($r.build)"
 ))
-| flatten'
+| flatten
+'
 
 COLUMNS=(CODE NAME TYPE DATE VERSION BUILD)
 {
@@ -37,5 +40,5 @@ COLUMNS=(CODE NAME TYPE DATE VERSION BUILD)
 			--arg products "${PRODUCTS_LIST[*]}" \
 			--arg release_types "$RELEASE_TYPES_RE" \
 			--argjson last_n "$LAST_N" \
-			"$JQ_PROG[]" -r
+			". $JQ_PROG []" -r
 } | column -Lt -s $'\t'
