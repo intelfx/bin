abeee1286621009738c8a878c369db22447b6a59
Ivan Shapovalov <intelfx@intelfx.name>
2026-06-28 20:03:07 +0300

    devel: tag-bcachefs.sh: sync in opposite direction, crudely (post-1.38.3)

--- devel/tag-bcachefs.sh
+++ devel/tag-bcachefs.sh
@@ -87,11 +87,118 @@ extract_one_revision() {
 	set_one_revision "$kind" "$kernel_ref" "$kernel_rev"
 }
 
+sync_one_revision() {
+	local kind="$1"
+	local tools_ref="$2"
+	local kernel_ref="$3"
+
+	local force_update=0
+	local arg
+	for arg in "${@:4}"; do
+		case "$arg" in
+		--force-update) force_update=1 ;;
+		*) die "extract_one_revision: invalid arguments: ${*@Q}" ;;
+		esac
+	done
+
+	tools_rev="$(git -C "$BCACHEFS_TOOLS_DIR" rev-parse --short "$tools_ref^{commit}")"
+	kernel_rev_existing="$(git -C "$BCACHEFS_KERNEL_DIR" rev-parse --verify --short "$kernel_ref^{commit}" 2>/dev/null)" || kernel_rev_existing=""
+
+	# superficial check -- if a tag exists, do not bother replaying source changes, assume it's good
+	if [[ $kernel_rev_existing && $kind == tag ]]; then
+		log "tools/$tools_ref ($tools_rev) => kernel/$kernel_ref ($kernel_rev_existing) exists"
+		return 0
+	fi
+
+	git -C "$BCACHEFS_TOOLS_DIR" checkout -f "$tools_ref"
+	git -C "$BCACHEFS_KERNEL_DIR" checkout -f "${parent_kernel_ref:?}"
+	rsync \
+		-r -lDH --chmod=ugo=rwX --delete-after \
+		--exclude 'util/mean_and_variance_test.c' \
+		"$BCACHEFS_TOOLS_DIR/fs/" "$BCACHEFS_KERNEL_DIR/fs/bcachefs/"
+
+	git update-index --refresh -q &>/dev/null ||:
+	git -C "$BCACHEFS_KERNEL_DIR" add -A fs/bcachefs/
+
+	if git -C "$BCACHEFS_KERNEL_DIR" diff-index --quiet --cached HEAD --; then
+		log "tools/$tools_ref ($tools_rev) => no change"
+		return 0
+	fi
+
+	# read author identity and timestamp of the release commit
+	git -C "$BCACHEFS_TOOLS_DIR" log "$tools_rev" \
+		-1 \
+		--format=$'%an\t%ae\t%ad\n' \
+	| IFS=$'\t' read -r c_name c_email c_date
+
+	# read commit hash and subject of the last relevant commit
+	git -C "$BCACHEFS_TOOLS_DIR" log "$tools_rev" \
+		-1 \
+		--format=$'%h\t%s\n' \
+		-- fs/ \
+	| IFS=$'\t' read -r cc_hash cc_subject
+
+	# reset both author and committer details to make the commit ID deterministic
+	# GIT_AUTHOR_NAME="$c_name" GIT_AUTHOR_EMAIL="$c_email" \
+	# GIT_COMMITTER_NAME="$c_name" GIT_COMMITTER_EMAIL="$c_email" \
+	GIT_AUTHOR_DATE="$c_date" \
+	GIT_COMMITTER_DATE="$c_date" \
+	git -C "$BCACHEFS_KERNEL_DIR" commit -m "Update bcachefs sources to $cc_hash $cc_subject"
+
+	kernel_rev="$(git -C "$BCACHEFS_KERNEL_DIR" rev-parse --short HEAD)"
+
+	if [[ $kernel_rev_existing && $kernel_rev_existing != $kernel_rev ]]; then
+		if [[ $kind == tag ]]; then
+			log "tools/$tools_ref ($tools_rev) => kernel/$kernel_ref ($kernel_rev) != $kernel_rev_existing, bailing out"
+			exit 1
+		fi
+		log "tools/$tools_ref ($tools_rev) => kernel/$kernel_ref ($kernel_rev) != $kernel_rev_existing, updating"
+	elif [[ $kernel_rev_existing == $kernel_rev ]]; then
+		log "tools/$tools_ref ($tools_rev) => kernel/$kernel_ref ($kernel_rev_existing) exists"
+		return 0
+	else
+		log "tools/$tools_ref ($tools_rev) => kernel/$kernel_ref ($kernel_rev)"
+	fi
+	set_one_revision "$kind" "$kernel_ref" "$kernel_rev"
+}
+
+# $1 is greater than $2
+vergreater() {
+	local ret
+	ret="$(vercmp "${1##v}" "${2##v}")"
+	(( ret > 0 ))
+}
+
 git -C "$BCACHEFS_TOOLS_DIR" ls-refs --format '%(refname:short)' 'refs/tags/v*' \
 | sort -V \
 | while IFS='' read -r tag; do
+	if vergreater "$tag" "v1.38.3"; then
+		break
+	fi
 	extract_one_revision tag "$tag" "bcachefs/${tag#v}"
 	last_tools_ref="$tag"
+	last_kernel_ref="bcachefs/${tag#v}"
 done
-extract_one_revision branch "$last_tools_ref" bcachefs-tools/release --force-update
-extract_one_revision branch "$BCACHEFS_TOOLS_REMOTE/master" bcachefs-tools/master --force-update
+# extract_one_revision branch "$last_tools_ref" bcachefs-tools/release --force-update
+# extract_one_revision branch "$BCACHEFS_TOOLS_REMOTE/master" bcachefs-tools/master --force-update
+
+git -C "$BCACHEFS_TOOLS_DIR" ls-refs --format '%(refname:short)' 'refs/tags/v*' \
+| sort -V \
+| while IFS='' read -r tag; do
+	if ! vergreater "$tag" "v1.38.3"; then
+		continue
+	fi
+
+	# this is used as the parent of the generated commit
+	# set it here so that on the last iteration, both the last release tag
+	# and the release branch will all use the same parent (previous release)
+	parent_kernel_ref="$last_kernel_ref"
+	sync_one_revision tag "$tag" "bcachefs/${tag#v}"
+	last_tools_ref="$tag"
+	last_kernel_ref="bcachefs/${tag#v}"
+done
+sync_one_revision branch "$last_tools_ref" bcachefs-tools/release
+
+# master is on top of the last release, so advance the parent here
+parent_kernel_ref="$last_kernel_ref"
+sync_one_revision branch "$BCACHEFS_TOOLS_REMOTE/master" bcachefs-tools/master
