ff04c93edffa726611b5a1fe7bdb0e1ce7b3a24f
Ivan Shapovalov <intelfx@intelfx.name>
2026-07-24 21:56:47 +0300

    devel: unfuck.sh: add patches for v7.1

--- devel/unfuck.sh
+++ devel/unfuck.sh
@@ -460,6 +460,39 @@ v7.0)
 		# work/gvt-workaround-6.18 \
 		# work/i915-fastboot-revert-6.18 \
 	;;
+
+v7.1)
+	# bcachefs
+	make_merge \
+		work/bcachefs-7.1 \
+		"$(git describe --tags --match 'bcachefs/*' --exact-match --always bcachefs-tools/release)" \
+		# EOL
+
+	# main
+	make_merge \
+		work/iwlwifi-lar-v3-7.0 \
+		work/amd-energy-6.18 \
+		work/btrfs-remove-ghost-subvolume-7.0 \
+		work/btrfs-allocation-hint-7.0 \
+		work/tsc-directsync-7.1 \
+		work/no-jobserver-exec-6.19 \
+		work/kbuild-6.18 \
+		work/em7565-ids-6.18 \
+		work/zswap-writeback-6.18 \
+		work/acpi-osc-7.0 \
+		work/logitech-hidpp-6.19 \
+		work/fs-6.18-noop \
+		work/cddl-7.1 \
+		work/zfs-7.1 \
+		work/fonts-7.1 \
+		work/intel-rapl-hack-6.18 \
+		work/pf-no-teo-7.0 \
+		work/perf-zstd-6.18 \
+		work/mitigations-7.0 \
+		# work/gvt-failsafe-6.18 \
+		# work/gvt-workaround-6.18 \
+		# work/i915-fastboot-revert-6.18 \
+	;;
 esac
 
 Trace git branch -f "${target[patch_prefix]}${minor}"
