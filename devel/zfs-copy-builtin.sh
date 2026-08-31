363b275f17eb6aa8aaaafe6d15d9fc6f55818af8
Ivan Shapovalov <intelfx@intelfx.name>
2026-08-31 07:30:07 +0300

    devel: zfs-copy-builtin.sh: update Kbuild integration commit reminder

--- devel/zfs-copy-builtin.sh
+++ devel/zfs-copy-builtin.sh
@@ -90,5 +90,5 @@ fi
 
 log "done. now you can build the kernel with ZFS support."
 log "don't forget to add Kbuild integration (c3373c78b9dcbe5cabd94fcba2bcabd3464f6784)."
-log "                (...or, for zfs 2.4.3+, b1ab37bd61713ecb4faf96f9d21875deb0725d04)"
+log "                (...or, for zfs 2.4.3+, 44856ee28236b570fe8164d7707dd14193e1110c)"
 log "make sure you enable ZFS support (CONFIG_ZFS) before building."
